0a43615a4dbbbf047a71136b63be25b52e925d3b
Mz <>
2025-11-19 08:26:22 -0600

    .

--- extrapatches/one_hit_ko.asm
+++ extrapatches/one_hit_ko.asm
@@ -1,5 +1,6 @@
-;
-; Can't get Mushroom or Cape.
+;Experimental
+
+; Mushroom or Cape doesnh't protect you.
 ; One hit still gets you, even on Yoshi.
 ;
 if read1($00FFD5) == $23
@@ -12,6 +13,8 @@ else
     !bank = $000000
 endif	
 !PowerupAddress = $19|!addr
+!OnYoshiAddr = $187A|!addr
+!OnYoshiOW = $0DC1|!addr
 
 org $00F5B7 
    autoclean JML Main
@@ -20,10 +23,13 @@ freecode
 Main:
     LDA #$00
     STA !PowerupAddress ; Store it to the RAM address $7E0019
+    STA !OnYoshiAddr
+    STA !OnYoshiOW
     JML $00F5BD
 
 
 ;Munchers and spikes hit you even on Yoshi.
 org $00F124
   NOP #2 ; Erase the Yoshi-checking branch
+
 

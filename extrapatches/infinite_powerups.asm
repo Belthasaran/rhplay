18004d0de0db9db60cc4b61f26271c02c76ce4d0
Mz <>
2025-11-20 04:43:48 -0600

    try new version of infinite_powerups.  previous one crashes on boo ring

--- extrapatches/infinite_powerups.asm
+++ extrapatches/infinite_powerups.asm
@@ -1,20 +1,47 @@
-if read1($00FFD5) == $23
-    sa1rom
-    !addr = $6000
-    !bank = $000000
-else
-    lorom
-    !addr = $0000
-    !bank = $000000
-endif	
-!PowerupAddress = $19|!addr
+; Experimental  new version of OhKo based on   Kevinskie555/Smallhacker  knockback patch
+;Sound effect to play when hurt
+!sfx = $20
+!sfxPort = $1DF9|!addr
+;Speed to be knocked back with
+!knockSpeed = $40
 
-org $00F5B7 
-   autoclean JML Main
+        !addr = $0000
+if read1($00FFD5) == $23
+	sa1rom
+	!addr = $6000
+endif
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Hijack the hurt routine
+org $00F5D5
+    autoclean JML NewRoutine
 
 freecode
-Main:
-    LDA #$02      
-    STA !PowerupAddress ; Store it to the RAM address $7E0019
-    JML $00F5BD
+NewRoutine:
+	LDA #!sfx
+	STA !sfxPort
+	;LDA.b #($100-!knockSpeed)
+	;STA $7D
+        LDA.b #$02  ;+
+        STA $19     ;+
+	LDA $76
+	BNE Other
+	LDA #!knockSpeed
+	BRA Continue
+    
+Other:
+;	LDA.b #($100-!knockSpeed)
+    LDA.b #$02
+Continue:
+    STA $19
+;	STA $7B
+
+MarioFlash:
+	LDA #$7F
+	STA $1497|!addr
+;;
+;        jml $00F606
+	RTL
+
 

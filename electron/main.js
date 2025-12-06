985839d65185bb34e200fad9b85d90cadfdae266
Mz <>
2025-12-06 07:52:23 -0600

    .

--- electron/main.js
+++ electron/main.js
@@ -9,7 +9,7 @@ const { registerDatabaseHandlers } = require('./ipc-handlers');
 const StartupPathValidator = require('./startup-path-validator');
 const { queueRhpakPath, drainRhpakQueue } = require('./rhpak-queue');
 
-const DATABASE_FILES = ['clientdata.db', 'rhdata.db', 'patchbin.db', 'resource.db', 'screenshot.db'];
+const DATABASE_FILES = ['clientdata.db', 'rhdata.db', 'patchbin.db', 'resource.db', 'screenshot.db', 'thumbnail_cache.db'];
 let handlersRegistered = false;
 let mainWindow = null;
 let currentMode = 'app';

9e99742b32eb3f2416ffe06f8062f80879689268
Mz <>
2025-12-24 09:39:01 -0600

    name_klcdx files

---
+++ pytools/name_kldcx_entries.py
@@ -0,0 +1,15 @@
+import os
+import re
+
+for de in os.listdir("."):
+   m = re.match('KLDCX_(\d+)_([^.]+).bps',de)
+   if not(m):
+       continue
+   fromname = de
+   toname = 'KLDCX level ' + m.group(1) + ' by ' + m.group(2) + ' [2022-12-08] [Kaizo] (SMW Hack) (Contest Entry).bps'
+   toname = " ".join(toname.split(" "))
+   print("fromname = "  + fromname)
+   print("toname = " + toname + "\n")
+   os.rename(fromname,toname)
+   
+

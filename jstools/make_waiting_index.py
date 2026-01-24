84f237e4e32308e8249a71816b9e0b20cd02c72d
Mz <>
2026-01-24 14:51:43 -0600

    make a CSV listing waiting game json entries

---
+++ jstools/make_waiting_index.py
@@ -0,0 +1,30 @@
+import os
+import json
+import pathlib
+import re
+import csv
+
+basedir = pathlib.Path(__file__).parent.resolve()
+gamesdir = os.path.join(basedir, 'smwc_world','games')
+
+cols = ['moderated','time','date','gameid','name','demo','sa1','collab','author','authors','submitter','combinedtype','length','fields_type','difficulty','warnings','url','section','tags','bps_files','json_files','']
+items = {}
+
+with open(os.path.join(gamesdir, 'waiting_index.csv'), 'w', newline='') as fw:
+    writer = csv.DictWriter(fw, fieldnames=cols)
+    writer.writeheader()
+    for de in os.listdir(gamesdir):
+        if not(re.match(r'.*\.json$', de)):
+            continue
+        print(de)
+        f = open(de, 'r')
+        j = json.load(f)
+        itemdata = {
+           k: j[k]  for k in cols if k in j
+        }
+        if 'url' in itemdata:
+            itemdata["url"] = itemdata["url"].replace('https://www.smwcentral.net/','/')
+        writer.writerow(itemdata)
+        f.close()
+
+

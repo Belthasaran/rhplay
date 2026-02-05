e57cbaed84d500d4fd9516f13225add47b32d353
Mz <>
2026-02-05 14:52:37 -0600

    full path our read operations - use the full filename path when opening the file

--- jstools/make_waiting_index.py
+++ jstools/make_waiting_index.py
@@ -10,14 +10,14 @@ gamesdir = os.path.join(basedir, 'smwc_world','games')
 cols = ['moderated','time','date','gameid','name','demo','sa1','collab','author','authors','submitter','combinedtype','length','fields_type','difficulty','warnings','url','section','tags','bps_files','json_files','']
 items = {}
 
-with open(os.path.join(gamesdir, 'waiting_index.csv'), 'w', newline='') as fw:
+with open(os.path.join(basedir,'smwc_world', 'waiting_index.csv'), 'w', newline='') as fw:
     writer = csv.DictWriter(fw, fieldnames=cols)
     writer.writeheader()
     for de in os.listdir(gamesdir):
         if not(re.match(r'.*\.json$', de)):
             continue
         print(de)
-        f = open(de, 'r')
+        f = open(os.path.join(gamesdir,de), 'r')
         j = json.load(f)
         itemdata = {
            k: j[k]  for k in cols if k in j

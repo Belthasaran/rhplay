567c8965bfd82de4f7d8c26c3c1a87477197d70f
Mz <>
2026-02-16 23:25:23 -0600

    release preparation for 0.1.20beta

---
+++ release1-update-mf.sh
@@ -0,0 +1,8 @@
+
+
+ enode.sh jsutils/update_coremf.js electron/coremanifest.json --target beta/RHPLAY/win64/portable --exe ~/release/0.1.21beta/RHTools-0.1.21-beta-portable.exe --ardrive-drive-id 58677413-8a0c-4982-944d-4a1b40454039 --ardrive-folder-id eb87554f-6669-433b-9fb6-1204e018031c
+
+
+ enode.sh jsutils/update_coremf.js electron/coremanifest.json --target beta/RHPLAY/linux64/AppImage --exe ~/release/0.1.21beta/RHTools-0.1.21-beta.AppImage  --ardrive-drive-id 58677413-8a0c-4982-944d-4a1b40454039 --ardrive-folder-id eb87554f-6669-433b-9fb6-1204e018031c
+
+
